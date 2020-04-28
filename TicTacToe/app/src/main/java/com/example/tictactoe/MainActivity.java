e72e1fd03a3be09d49e8ba9c22139983752590aa
Farid Islam <56597451+Farid-Islam@users.noreply.github.com>
2020-04-28 19:59:56 +0600

    tic tac toe uploaded

---
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,281 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.SystemClock;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button a1,a2,a3,b1,b2,b3,c1,c2,c3,player_1;
+    private Button[] bArray;
+    private boolean turn = true;
+    private int turn_count = 0;
+    private TextView turnShow;
+    private Button resetButton;
+    private TextView o_wins,x_wins,draw;
+    private int zeroWinsCount = 0,xWinsCount = 0,drawCount = 0;
+    private MediaPlayer sound,gameOverSound;
+
+    private Chronometer chronometer_0,chronometer_x;
+    private long chrono_x_count,chrono_0_count;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findId();
+        bArray = new Button[]{a1,a2,a3,b1,b2,b3,c1,c2,c3};
+        for(Button b : bArray){
+            b.setOnClickListener(this);
+        }
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                chronometer_0.stop();
+                chronometer_x.stop();
+                chronometer_0.setBase(SystemClock.elapsedRealtime());
+                chronometer_x.setBase(SystemClock.elapsedRealtime());
+                chrono_0_count = 0;
+                chrono_x_count = 0;
+
+                sound.start();
+                turn = true;
+                turn_count = 0;
+                turnShow.setText("Turn: 0-Player");
+                enableIsAllButton(true);
+                bArray[0].setText(null);
+                bArray[1].setText(null);
+                bArray[2].setText(null);
+                bArray[3].setText(null);
+                bArray[4].setText(null);
+                bArray[5].setText(null);
+                bArray[6].setText(null);
+                bArray[7].setText(null);
+                bArray[8].setText(null);
+                bArray[0].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[1].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[2].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[3].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[4].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[5].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[6].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[7].setBackgroundColor(Color.parseColor("#64aea8a8"));
+                bArray[8].setBackgroundColor(Color.parseColor("#64aea8a8"));
+
+            }
+        });
+
+        sound = MediaPlayer.create(this,R.raw.multimedia_button_click_004);
+        gameOverSound = MediaPlayer.create(this,R.raw.game_over_sound);
+
+        chronometer_0.setFormat("0-Time: %s");
+        chronometer_x.setFormat("X-Time: %s");
+        chronometer_0.setBase(SystemClock.elapsedRealtime());
+        chronometer_x.setBase(SystemClock.elapsedRealtime());
+
+    }
+
+    private void findId() {
+
+        a1 = findViewById(R.id.id_1);
+        a2 = findViewById(R.id.id_2);
+        a3 = findViewById(R.id.id_3);
+        b1 = findViewById(R.id.id_4);
+        b2 = findViewById(R.id.id_5);
+        b3 = findViewById(R.id.id_6);
+        c1 = findViewById(R.id.id_7);
+        c2 = findViewById(R.id.id_8);
+        c3 = findViewById(R.id.id_9);
+
+        o_wins = findViewById(R.id.o_wins_id);
+        x_wins = findViewById(R.id.x_wins_id);
+        draw = findViewById(R.id.draw_id);
+
+        turnShow = findViewById(R.id.turnShow_id);
+        resetButton = findViewById(R.id.resetButton_id);
+        player_1 = findViewById(R.id.player_1_id);
+
+        chronometer_0 = findViewById(R.id.o_chronometer_id);
+        chronometer_x = findViewById(R.id.x_chronometer_id);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        sound.start();
+        Button b = (Button) v;
+        buttonClicked(b);
+
+    }
+
+    private void buttonClicked(Button b) {
+
+        if(turn){
+            chronometer_x.setBase(SystemClock.elapsedRealtime() - chrono_x_count);
+            chronometer_x.start();
+
+            if(turn_count != 0){
+                chronometer_0.stop();
+                chrono_0_count = SystemClock.elapsedRealtime() - chronometer_0.getBase();
+            }
+
+            b.setText("0");
+        }else{
+            chronometer_0.setBase(SystemClock.elapsedRealtime() - chrono_0_count);
+            chronometer_0.start();
+
+            chronometer_x.stop();
+            chrono_x_count = SystemClock.elapsedRealtime() - chronometer_x.getBase();
+            b.setText("X");
+        }
+        turn_count++;
+        //b.setBackgroundColor(Color.GREEN);
+        b.setClickable(false);
+        // change the turn value
+        turn = ! turn;
+
+        if(turn){
+            turnShow.setText("Turn: 0-Player");
+        }else{
+            turnShow.setText("Turn: X-Player");
+        }
+        checkForWinner();
+
+    }
+
+    private void checkForWinner() {
+        boolean there_is_a_winner = false;
+
+        if(a1.getText() == a2.getText() && a2.getText() == a3.getText() && !a1.isClickable()){
+            a1.setBackgroundColor(Color.parseColor("#6404f714"));
+            a2.setBackgroundColor(Color.parseColor("#6404f714"));
+            a3.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+        else if(b1.getText() == b2.getText() && b2.getText() == b3.getText() && !b1.isClickable()){
+            b1.setBackgroundColor(Color.parseColor("#6404f714"));
+            b2.setBackgroundColor(Color.parseColor("#6404f714"));
+            b3.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+        else if(c1.getText() == c2.getText() && c2.getText() == c3.getText() && !c1.isClickable()){
+            c1.setBackgroundColor(Color.parseColor("#6404f714"));
+            c2.setBackgroundColor(Color.parseColor("#6404f714"));
+            c3.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+
+        else if(a1.getText() == b1.getText() && b1.getText() == c1.getText() && !a1.isClickable()){
+            a1.setBackgroundColor(Color.parseColor("#6404f714"));
+            b1.setBackgroundColor(Color.parseColor("#6404f714"));
+            c1.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+        else if(a2.getText() == b2.getText() && b2.getText() == c2.getText() && !a2.isClickable()){
+            a2.setBackgroundColor(Color.parseColor("#6404f714"));
+            b2.setBackgroundColor(Color.parseColor("#6404f714"));
+            c2.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+        else if(a3.getText() == b3.getText() && b3.getText() == c3.getText() && !a3.isClickable()){
+            a3.setBackgroundColor(Color.parseColor("#6404f714"));
+            b3.setBackgroundColor(Color.parseColor("#6404f714"));
+            c3.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+
+        else if(a1.getText() == b2.getText() && b2.getText() == c3.getText() && !a1.isClickable()){
+            a1.setBackgroundColor(Color.parseColor("#6404f714"));
+            b2.setBackgroundColor(Color.parseColor("#6404f714"));
+            c3.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+        else if(a3.getText() == b2.getText() && b2.getText() == c1.getText() && !a3.isClickable()){
+            a3.setBackgroundColor(Color.parseColor("#6404f714"));
+            b2.setBackgroundColor(Color.parseColor("#6404f714"));
+            c1.setBackgroundColor(Color.parseColor("#6404f714"));
+            there_is_a_winner = true;
+        }
+
+        if(there_is_a_winner){
+
+            chronometer_0.stop();
+            chronometer_x.stop();
+
+            gameOverSound.start();
+            if(!turn){
+                zeroWinsCount++;
+                o_wins.setText(""+zeroWinsCount);
+                turnShow.setText("0-Win");
+                toast("0 win");
+            }else {
+                xWinsCount++;
+                x_wins.setText(""+xWinsCount);
+                turnShow.setText("X-Win");
+                toast("X wins");
+            }
+            enableIsAllButton(false);
+
+        }else if(turn_count == 9){
+
+            chronometer_0.stop();
+            chronometer_x.stop();
+
+            gameOverSound.start();
+            drawCount++;
+            draw.setText(""+drawCount);
+            turnShow.setText("Draw");
+            toast("Draw");
+        }
+
+
+    }
+
+    private void enableIsAllButton(boolean enable) {
+        for(Button b : bArray){
+            b.setClickable(enable);
+        }
+    }
+
+    private void toast(String message) {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Quit the game?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
+
+}
